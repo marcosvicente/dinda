015a8d319cf0e7eb21ec0c9d5917b642d53a6670
Marcos Vicente <marcos.vicente@wakai.com.br>
2018-08-09 15:22:16 -0300

    response can not be nil

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,1 +1,11 @@
-require "rails_helper"
+#!/usr/bin/env ruby
+
+require "spec_helper"
+require_relative '../lib/app'
+
+describe "get data from github api" do
+  it "response can not be nil" do
+    get_data_api
+    expect(@response).to_not be_nil
+  end
+end
